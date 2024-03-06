ed99eb05480228bb89ad516455877bc72afcef55
Waibhav <waibhav@Netopss-MacBook-Pro.local>
2024-03-06 18:26:01 +0530

    added new service for faq handling

---
+++ chatbot/faq/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from modules.faq_handler import faq
+
+
+app = Flask(__name__)
+faq_obj = faq()
+
+
+@app.route('/ingest/faq', methods = ['POST'])
+def ingest_content():
+    data = request.get_json()
+    print(data)
+    faqs =  data['faqs']
+    faq_obj.load_faq(faqs)
+    return jsonify({'message': 'loaded the FAQs'}), 200
+
+@app.route('/faq/query', methods=['POST'])
+def generate_answers():
+    data = request.get_json()
+    print(data)
+    #collection_name = data.get('collection_name', '')
+    query = data.get('query', '')
+    faq_response, score = faq_obj.query(query)
+    if faq_response:
+        return jsonify({'generated_ans': faq_response, 'closest context' : "faq", "score" : score}), 200 
+    else:
+        return jsonify({'generated_ans': '', 'closest context' : "faq", "score" : 0}), 404 
+    
+
+if __name__ == '__main__':
+    print("running on 5000 port")
+    app.run(debug=False, host='0.0.0.0', port=5001)
---
+++ chatbot/faq/modules/faq_handler.py
@@ -0,0 +1,55 @@
+from haystack.document_stores import InMemoryDocumentStore
+from haystack.nodes import EmbeddingRetriever
+import pandas as pd
+from haystack.pipelines import FAQPipeline
+from haystack.utils import print_answers
+import os
+import json
+
+class faq:
+    
+    def __init__(self) -> None:
+        with open('conf/config.json') as config_file:
+            self.conf = json.load(config_file)
+        self.document_store = InMemoryDocumentStore()
+        self.retriever = EmbeddingRetriever(
+            document_store=self.document_store,
+            embedding_model="sentence-transformers/all-MiniLM-L6-v2",
+            use_gpu=True,
+            scale_score=False,
+        )
+        self.ques_list = []
+        self.ans_list = []
+        
+        self.pipe = []
+
+    def load_faq(self, faq_json):
+        faq_data = pd.DataFrame()
+        print(type(faq_json))
+        print(faq_json)
+        for obj in faq_json:
+            print(obj)
+            self.ques_list.append(obj['heading'])
+            self.ans_list.append(obj['value'])
+        faq_data['question'] = self.ques_list
+        faq_data["question"] = faq_data["question"].apply(lambda x: x.strip())
+        faq_data['answer'] = self.ans_list
+
+        questions = list(faq_data["question"].values)
+        faq_data["embedding"] = self.retriever.embed_queries(queries=questions).tolist()
+        faq_data = faq_data.rename(columns={"question": "content"})
+        docs_to_index = faq_data.to_dict(orient="records")
+        self.document_store.write_documents(docs_to_index)
+
+        self.pipe = FAQPipeline(retriever=self.retriever)
+
+    def query(self, ques):
+        prediction = self.pipe.run(query=ques, params={"Retriever": {"top_k": 1}})
+        #print_answers(prediction, details="medium")
+        answer =  prediction['answers'][0]
+        if answer.score > float(os.getenv('haystack_faq_cutoff', self.conf["haystack_faq_cutoff"])):
+            return answer.answer, answer.score
+        else:
+            return False, False
+    
+
