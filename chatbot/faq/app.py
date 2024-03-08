b7466428dd84281fd31ff6a4b0057c134fd78549
Waibhav <waibhav@Netopss-MacBook-Pro.local>
2024-03-08 11:27:19 +0530

    added log

--- chatbot/faq/app.py
+++ chatbot/faq/app.py
@@ -9,7 +9,6 @@ faq_obj = faq()
 @app.route('/ingest/faq', methods = ['POST'])
 def ingest_content():
     data = request.get_json()
-    print(data)
     faqs =  data['faqs']
     faq_obj.load_faq(faqs)
     return jsonify({'message': 'loaded the FAQs'}), 200
@@ -22,6 +21,7 @@ def generate_answers():
     query = data.get('query', '')
     faq_response, score = faq_obj.query(query)
     if faq_response:
+        print({'generated_ans': faq_response, 'closest context' : "faq", "score" : score})
         return jsonify({'generated_ans': faq_response, 'closest context' : "faq", "score" : score}), 200 
     else:
         return jsonify({'generated_ans': '', 'closest context' : "faq", "score" : 0}), 404 
