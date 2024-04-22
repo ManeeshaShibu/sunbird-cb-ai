d69496281fee3394d7252281092bd58054a9f0fd
Waibhav <waibhav@Netopss-MacBook-Pro.local>
2024-04-22 11:45:08 +0530

    updated ES connection endpoint to without auth

--- chatbot/chat_ui/modules/es/connection.js
+++ chatbot/chat_ui/modules/es/connection.js
@@ -3,9 +3,10 @@ var elasticsearch=require('elasticsearch');
 
 var client = new elasticsearch.Client( {  
   hosts: [
-    'http://'+process.env.APP_ES_USERNAME+':'+process.env.APP_ES_PASSWORD+'@'+process.env.APP_ES_HOST + ':' + process.env.APP_ES_PORT
+    //'http://'+process.env.APP_ES_USERNAME+':'+process.env.APP_ES_PASSWORD+'@'+process.env.APP_ES_HOST + ':' + process.env.APP_ES_PORT
+    'http://' + process.env.APP_ES_HOST + ':' + process.env.APP_ES_PORT
   ]
 });
 
-console.log('http://'+process.env.APP_ES_USERNAME+':'+process.env.APP_ES_PASSWORD+'@'+process.env.APP_ES_HOST + ':' + process.env.APP_ES_PORT)
+console.log('http://'+process.env.APP_ES_HOST + ':' + process.env.APP_ES_PORT)
 module.exports = client;
