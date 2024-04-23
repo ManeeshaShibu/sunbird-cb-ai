c6f16f547f1a7d62fb649ab485fba9c01a855571
Waibhav <waibhav@Netopss-MacBook-Pro.local>
2024-04-23 16:02:45 +0530

    hardcoded es endpoint for testing

--- chatbot/chat_ui/modules/es/connection.js
+++ chatbot/chat_ui/modules/es/connection.js
@@ -4,7 +4,7 @@ var elasticsearch=require('elasticsearch');
 var client = new elasticsearch.Client( {  
   hosts: [
     //'http://'+process.env.APP_ES_USERNAME+':'+process.env.APP_ES_PASSWORD+'@'+process.env.APP_ES_HOST + ':' + process.env.APP_ES_PORT
-    'http://' + process.env.APP_ES_HOST || '192.168.3.211'  + ':' + process.env.APP_ES_PORT || '9200'
+    'http://' +  '192.168.3.211'  + ':' + '9200'
   ]
 });
 
